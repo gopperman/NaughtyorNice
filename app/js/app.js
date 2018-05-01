5a74abdcec87a1b84689abbc1708b44a1b7effed
Greg Opperman <greg@patientdiscovery.com>
2018-05-01 16:28:22 -0400

    Prefixing urls with gh repo name

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,8 @@
 // Declare app level module which depends on filters, and services
 angular.module('NoN', []).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/', {templateUrl: 'app/partials/main.html', controller: 'mainController'});
-    $routeProvider.when('/naughty', {templateUrl: 'app/partials/naughty.html', controller: 'MyCtrl'});
-    $routeProvider.when('/nice', {templateUrl: 'app/partials/nice.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: ''});
+    $routeProvider.when('/', {templateUrl: 'NaughtyorNice/app/partials/main.html', controller: 'mainController'});
+    $routeProvider.when('/naughty', {templateUrl: 'NaughtyorNice/app/partials/naughty.html', controller: 'MyCtrl'});
+    $routeProvider.when('/nice', {templateUrl: 'NaughtyorNice/app/partials/nice.html', controller: 'MyCtrl2'});
+    $routeProvider.otherwise({redirectTo: '/NaughtyorNice/'});
   }]);
